0223c6145e97e47a8a8cba1a09ae6b0b6a9f9c5e
meriadec <meriadec.pillet@gmail.com>
2016-02-22 23:17:03 +0100

    basic "editor" (lol)

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react'
 //import mjml from 'mjml'
 
+import '../styles/Editor.css'
+
 export default class Home extends Component {
+
+  state = {
+    content: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({ content: e.target.value })
+  }
+
   render () {
+    const { content } = this.state
+
     return (
       <div>
-        {'mjml-desktop'}
+        <div className='Editor'>
+          <div className='Editor-panel'>
+            <textarea value={content} onChange={this.handleChange} />
+          </div>
+          <div className='Editor-panel'>
+            {content}
+          </div>
+        </div>
       </div>
     )
   }
+
 }
