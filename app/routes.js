aeb6365b5dcb816c35f21253aea0147daee0146a
meriadec <meriadec.pillet@gmail.com>
2016-02-27 14:26:17 +0100

    rename pages

--- app/routes.js
+++ app/routes.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
 import App from './components/App'
-import HomePage from './components/Home'
-import EditorPage from './components/Editor'
+import HomePage from './components/HomePage'
+import EditorPage from './components/EditorPage'
 
 export default (
   <Route path='/' component={App}>
