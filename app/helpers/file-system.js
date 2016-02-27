cbd7fcfc5acf3aef5107cda59f8fcd13d20f1fd7
Guillaume Badi <gbadi@student.42.fr>
2016-02-27 16:55:55 +0100

    save template

--- app/helpers/file-system.js
+++ app/helpers/file-system.js
@@ -23,6 +23,10 @@ export const readTemplates = (mjml) => promisify(fs.readdir)(mjml)
  */
 export const readConfig = (mjml) => require(path.join(mjml, '.mjml.json'))
 
+const checkOrCreate = (location) =>
+  promisify((location, cb) => fs.access(location, fs.R_OK | fs.W_OK, cb))(location)
+    .catch(() => promisify(fs.mkdir)(location))
+
 /*
  * Save an MJML template
  * template: json {
@@ -30,7 +34,7 @@ export const readConfig = (mjml) => require(path.join(mjml, '.mjml.json'))
  *  name
  * }
  */
-export const save = (template, location) => {
-  console.log(location)
-  return promisify(fs.writeFile)(path.join(location, `${template.get('name')}.json`), JSON.stringify(template.toJS(), null, 2))
-}
+export const save = (template, location) =>
+  checkOrCreate(location)
+    .then(() => promisify(fs.writeFile)(path.join(location, `${template.get('name')}.json`), JSON.stringify(template.toJS(), null, 2)))
+  
