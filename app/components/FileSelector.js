2cc9944e6bd7b47e4c12e2876d30d30dd80f22ef
Guillaume Badi <gbadi@student.42.fr>
2016-02-27 12:18:37 +0100

    home

---
+++ app/components/FileSelector.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react'
+
+class FileSelector extends Component {
+
+  render () {
+    return (
+      <div className='file-selector'>
+      </div>
+    )
+  }
+}
+
+export default FileSelector
--- app/components/Home.js
+++ app/components/Home.js
@@ -1,62 +1,19 @@
 import React, { Component } from 'react'
-import AceEditor from 'react-ace'
-import mjml2html from 'mjml/lib/mjml2html'
 
-import 'brace/mode/xml'
-import 'brace/theme/solarized_dark'
+import '../styles/Home.css'
 
-import '../styles/Editor.css'
-
-export default class Home extends Component {
-
-  static aceProps = {
-    $blockScrolling: true
-  }
-
-  state = {
-    content: ''
-  }
-
-  componentDidUpdate () {
-    let html
-    try {
-      html = mjml2html(this.state.content)
-    } catch (e) {
-      html = this._oldHtml || ''
-    }
-    const doc = this._iframe.contentDocument
-    const documentElement = doc.documentElement
-    documentElement.innerHTML = html
-    this._oldHtml = html
-  }
-
-  handleChange = (content) => {
-    this.setState({ content })
-  }
+class Home extends Component {
 
   render () {
-    const { content } = this.state
-
     return (
-      <div>
-        <div className='Editor'>
-          <div className='Editor-panel'>
-            <AceEditor
-              mode='xml'
-              theme='solarized_dark'
-              height={window.innerHeight}
-              value={content}
-              tabSize={2}
-              onChange={this.handleChange}
-              name='editor'
-              editorProps={Home.aceProps}/>
-          </div>
-          <div className='Editor-preview'>
-            <iframe id='preview' ref={(el) => this._iframe = el} />
-          </div>
-        </div>
+      <div className='container-home'>
+        <header>
+          <h1>MJML App.</h1>
+          <img width="200px" src="https://avatars1.githubusercontent.com/u/16115896?v=3" alt="mjml" />
+        </header>
       </div>
     )
   }
-
 }
+
+export default Home
