c4b2dc020358597184709d86c836baf7ab6dba60
meriadec <meriadec.pillet@gmail.com>
2016-03-01 01:11:11 +0100

    :eyeglasses:

--- app/index.js
+++ app/index.js
@@ -9,7 +9,7 @@ import { loadConfig } from './actions'
 import { readTemplates } from './actions/templates'
 
 import './styles/main.scss'
-import './actions/shortcuts'
+// import './actions/shortcuts'
 
 const store = configureStore()
 
