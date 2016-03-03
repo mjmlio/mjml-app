b7c9896aba6d6bf982c51deef735c2b47fc7c7da
Guillaume Badi <gbadi@student.42.fr>
2016-03-03 17:23:21 +0100

    typo

--- src/actions/send.js
+++ src/actions/send.js
@@ -16,12 +16,7 @@ export const send = (html) => (dispatch, getState) => {
     apiSecret: config.get('mjApiSecret'),
     name: config.get('userName'),
     sender: config.get('userEmail'),
-<<<<<<< HEAD
     to: 'gbadi@mailjet.com',
     html: html
-=======
-    to: config.get('userEmail'),
-    html
->>>>>>> c067597d4889892c8fed61b130ad90a892d3d64d
   }, sent, notSent)
 }
