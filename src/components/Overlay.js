0c9a77e3efcd99e2d49cb04fda0df55dd8aae04a
Guillaume Badi <gbadi@student.42.fr>
2016-03-05 16:51:50 +0100

    overlay works !

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,11 +1,24 @@
 
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import Portal from 'react-portal'
+
+import { loadTemplate, deleteTemplate } from '../actions/templates'
 
 import Button from './Button'
 
 import '../styles/Overlay.scss'
 
+@connect()
 class Overlay extends Component {
+
+  loadTemplate = template => () => {
+    this.props.dispatch(loadTemplate(template))
+  }
+
+  deleteTemplate = template => () => {
+    this.props.dispatch(deleteTemplate(template))
+  }
 
   renderOverlay (template) {
   
@@ -15,11 +28,11 @@ class Overlay extends Component {
           <i className='ion-pricetag' />
         </Button>
 
-        <Button className='button open'>
+        <Button className='button open' onClick={this.loadTemplate(template)}>
           <i className='ion-android-open' />
         </Button>
 
-        <Button className='button'>
+        <Button className='button' onClick={this.deleteTemplate(template)}>
           <i className='ion-trash-b' />
         </Button>
       </div>
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,32 +1,27 @@
 
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
 
 import Thumbnail from './Thumbnail'
 import Overlay from './Overlay'
-import { loadTemplate } from '../actions/templates'
 
 import '../styles/Tile.scss'
 
-@connect()
 class Tile extends Component {
 
-  loadTemplate = template => {
-    this.props.dispatch(loadTemplate(template))
+  state = {
+    overlay: false
   }
 
-  toggleOverlay = () => {
-    this._hover = !this._hover
-  }
+  showOverlay = () => this.setState({ overlay: true })
+  hideOverlay = () => this.setState({ overlay: false })
 
   render () {
 
     const { template } = this.props
 
-      // <div className='template' onClick={this.loadTemplate.bind(this, template)}>
     return (
-      <div className='template' onMouseEnter={this.toggleOverlay} onMouseLeave={this.toggleOverlay}>
-        <Overlay template={template} visible={this._hover}/>
+      <div className='template' onMouseEnter={this.showOverlay} onMouseLeave={this.hideOverlay}>
+        <Overlay template={template} visible={this.state.overlay}/>
         <div className='template-wrapper'>
           <Thumbnail template={template} />
         </div>
