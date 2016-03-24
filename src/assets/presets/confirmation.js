57e5c75b7aad435d0aebe66338f64c3573449b3e
meriadec <meriadec.pillet@gmail.com>
2016-03-24 10:43:12 +0100

    lint and export new presets

--- src/assets/presets/confirmation.js
+++ src/assets/presets/confirmation.js
@@ -155,6 +155,5 @@ export default {
       </mj-text>
     </mj-column>
   </mj-section>
-</mj-body>  
-`
+</mj-body>`
 }
--- src/assets/presets/worldly.js
+++ src/assets/presets/worldly.js
@@ -165,6 +165,5 @@ export default {
       </mj-text>
     </mj-column>
   </mj-section>
-</mj-body> 
-  `
+</mj-body>`
 }
