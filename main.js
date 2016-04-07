3034e9e79b92efda00fd75b018e6d1dc031d1a4b
meriadec <meriadec.pillet@gmail.com>
2016-04-07 14:55:03 +0200

    lint :eyeglasses:

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ app.on('ready', () => {
         { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
         { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
         { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' },
-        { label: 'Quit', accelerator: 'Command+Q', selector: 'Quit', click: function () {
+        { label: 'Quit', accelerator: 'Command+Q', selector: 'Quit', click: () => {
           app.quit()
         } }
       ] }
