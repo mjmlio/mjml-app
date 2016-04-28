a33d56b1b855747608a0811fb29af60abf1a4bbb
Guillaume Badi <gbadi@student.42.fr>
2016-04-28 09:43:23 +0200

    fixed brace and react-ace and removed online action-reporting

--- src/middlewares/action-report.js
+++ src/middlewares/action-report.js
@@ -1,40 +1,16 @@
 
-import fetch from 'superagent'
 import { remote } from 'electron'
 import fs from 'fs'
 import path from 'path'
 
-const reportUrl = 'http://localhost:3001/crash'
-
-/**
- * Send a report to the server
- *
- * @param {Object} e the error stack trace
- * @param {Object} state the current state
- * @param {String} the action name ('Function' if it is a function)
- * @returns {undefined}
- */
-const report2 = (e, state, action) =>
-  fetch
-    .post(reportUrl)
-    .send({
-      state,
-      action,
-      error: JSON.stringify({ stack: e.stack, message: e.message }),
-      version: remote.app.getVersion(),
-      appData: remote.app.getPath('appData'),
-    })
-    .end(() => false)
-
 const report = (e, state, action) =>
-  console.log(JSON.stringify(e, null, 4)) ||
   fs.writeFile(path.join(remote.app.getPath('home'), 'mjml-log'), JSON.stringify({
     state,
     action,
     error: JSON.stringify({ message: e.message, stack: e.stack }),
     version: remote.app.getVersion(),
     appData: remote.app.getPath('appData'),
-  }, null, 2), err => console.log(err || 'report created'))
+  }, null, 2))
 
 /**
  * actionReport middleware. post a crash report if an action throws an error
