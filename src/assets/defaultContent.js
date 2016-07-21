8d2830c7a16a3f237e3cacb5d54adfbc773158c9
meriadec <meriadec.pillet@gmail.com>
2016-07-21 22:26:10 +0200

    feat(core): update the base template to include mjml new features
    closes #56

--- src/assets/defaultContent.js
+++ src/assets/defaultContent.js
@@ -1,15 +1,22 @@
 export default `<mjml>
-  <mj-body>
-    <mj-container>
-      <mj-section>
-        <mj-column>
+<mj-head>
+  <mj-title>Hello world</mj-title>
+  <mj-attributes>
+    <mj-all />
+    <mj-text font-size="16" />
+  </mj-attributes>
+</mj-head>
+<mj-body>
+  <mj-container>
+    <mj-section>
+      <mj-column>
 
-          <mj-text font-size="40">
-            Hello world
-          </mj-text>
+        <mj-text font-size="40">
+          Hello world
+        </mj-text>
 
-        </mj-column>
-      </mj-section>
-    </mj-container>
-  </mj-body>
+      </mj-column>
+    </mj-section>
+  </mj-container>
+</mj-body>
 </mjml>`
