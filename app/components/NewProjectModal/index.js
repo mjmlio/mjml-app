e7d2602c88c5ef4cf48016a068a46846744fb2ec
meriadec <meriadec.pillet@gmail.com>
2017-03-22 21:05:37 +0100

    design the project create modal-

--- app/components/NewProjectModal/index.js
+++ app/components/NewProjectModal/index.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
+import os from 'os'
+import path from 'path'
 import { connect } from 'react-redux'
 
+const HOME_DIR = os.homedir()
+
 import Modal from 'components/Modal'
+import Button from 'components/Button'
 
 import {
   isModalOpened,
@@ -12,10 +17,26 @@ import './style.scss'
 
 @connect(state => ({
   isOpened: isModalOpened(state, 'newProject'),
-}), {
-  closeModal,
-})
+}), dispatch => ({
+  closeModal: () => dispatch(closeModal('newProject')),
+}))
 class NewProjectModal extends Component {
+
+  state = {
+    projectName: '',
+    projectLocation: HOME_DIR,
+  }
+
+  componentDidUpdate (prevProps) {
+    if (!prevProps.isOpened && this.props.isOpened) {
+      this._inputName.focus()
+    }
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+
+  }
 
   render () {
 
@@ -24,12 +45,82 @@ class NewProjectModal extends Component {
       closeModal,
     } = this.props
 
+    const {
+      projectName,
+      projectLocation,
+    } = this.state
+
+    const fullPath = (projectName && projectLocation)
+      ? path.join(projectLocation, projectName)
+      : null
+
     return (
       <Modal
+        className='NewProjectModal'
         isOpened={isOpened}
-        onClose={() => closeModal('newProject')}
+        onClose={closeModal}
       >
-        {'NewProjectModal'}
+        <form onSubmit={this.handleSubmit}>
+
+          <h2>{'New Project'}</h2>
+
+          <div className='flow-v-20'>
+            <div className='d-f ai-b'>
+              <div style={{ width: 150 }} className='fs-0'>
+                {'Project name:'}
+              </div>
+              <input
+                ref={n => this._inputName = n}
+                className='fg-1'
+                value={projectName}
+                onChange={e => this.setState({ projectName: e.target.value })}
+                placeholder='Project name'
+                type='text'
+                autoFocus
+              />
+            </div>
+
+            <div className='d-f ai-b'>
+              <div style={{ width: 150 }} className='fs-0'>
+                {'Location:'}
+              </div>
+              <div className='fg-1'>
+                <div className='d-f ai-s fg-1'>
+                  <input
+                    className='fg-1'
+                    value={projectLocation}
+                    onChange={e => this.setState({ projectLocation: e.target.value })}
+                    placeholder='Location'
+                    type='text'
+                    autoFocus
+                  />
+                  <Button ghost>
+                    {'Browse'}
+                  </Button>
+                </div>
+                {fullPath && (
+                  <div className='mt-10 t-small'>
+                    {'Project will be created at: '}
+                    <b className='c-white wb-ba'>
+                      {fullPath}
+                    </b>
+                  </div>
+                )}
+              </div>
+            </div>
+
+          </div>
+
+          <div className='ModalFooter'>
+            <Button primary>
+              {'Create'}
+            </Button>
+            <Button transparent onClick={closeModal}>
+              {'Cancel'}
+            </Button>
+          </div>
+
+        </form>
       </Modal>
     )
   }
