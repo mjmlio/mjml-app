773089f5031da17fc42e410e1b9c9e345b1751dc
meriadec <meriadec.pillet@gmail.com>
2017-03-31 17:21:23 +0200

    structure settings modal

--- app/pages/Project/SettingsModal.js
+++ app/pages/Project/SettingsModal.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import IconInfo from 'react-icons/md/settings'
 
 import { isModalOpened, closeModal } from 'reducers/modals'
 
@@ -29,11 +30,27 @@ class SettingsModal extends Component {
           {'Settings'}
         </div>
         <div className='d-f ai-fs'>
-          <div className='fg-1 mr-10'>
-            {'content'}
+          <div className='fg-1 mr-20'>
+
+            <h2 className='mb-20'>{'Editor'}</h2>
+
+            <div>
+              {'Wrap lines'}
+            </div>
+
+            <h2 className='mt-50 mb-20'>{'MJML'}</h2>
+
+            <div>
+              {'Wrap lines'}
+            </div>
+
           </div>
           <div style={{ width: 150 }}>
             <div className='version-box t-small flow-v-10'>
+
+              <div className='d-f jc-c'>
+                <IconInfo size={30} />
+              </div>
 
               <div className='d-f ai-c'>
                 <div className='fg-1'>
