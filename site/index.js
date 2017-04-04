5aeab61e60abf78131252a94626a91de2eb75d15
meriadec <meriadec.pillet@gmail.com>
2017-04-05 00:31:45 +0200

    start site

---
+++ site/index.js
@@ -0,0 +1,10 @@
+import './style.scss'
+
+const logo = document.getElementById('logo')
+logo.src = require('./logo-mjml.svg')
+
+const ghLogo = document.getElementById('ghLogo')
+ghLogo.src = require('./github.svg')
+
+const video = document.getElementById('video')
+video.src = require('./mjml-app.mp4')
---
+++ webpack.config.site.babel.js
@@ -0,0 +1,42 @@
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+const isProd = process.env.NODE_ENV === 'production'
+
+export default {
+  entry: './site/index.js',
+  output: {
+    path: 'dist',
+    filename: 'bundle-[hash].js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
+      { test: /\.mp4$/, loader: 'file' },
+      isProd ? {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(
+          'style-loader',
+          'css-loader!sass-loader',
+        ),
+      } : {
+        test: /\.scss$/,
+        loaders: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './site/template.html',
+    }),
+  ],
+}
