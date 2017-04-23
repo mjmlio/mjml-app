c78b26b76b9a7573c42e3abd828cecc8cbf47ff4
meriadec <meriadec.pillet@gmail.com>
2017-04-23 21:53:11 +0200

    work on site

--- site/index.js
+++ site/index.js
@@ -1,1 +1,10 @@
 import './style.scss'
+
+const REL_URL = 'https://api.github.com/repos/mjmlio/mjml-app/releases'
+
+fetch(REL_URL)
+  .then(res => res.json())
+  .then(res => {
+    const lastVersion = res[0]
+    document.getElementById('dl-btn-label').innerHTML = `&nbsp;- ${lastVersion.tag_name}`
+  })
