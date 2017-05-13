65e6290b55b5d62f328172fcce82d8929159becc
meriadec <meriadec.pillet@gmail.com>
2017-05-13 12:05:00 +0200

    Mailjet explanation on send modal

--- app/components/MailjetInfos.js
+++ app/components/MailjetInfos.js
@@ -3,6 +3,7 @@ import Collapse from 'react-collapse'
 import { shell } from 'electron'
 
 import IconInfo from 'react-icons/md/info'
+import LogoMailjet from 'components/icons/logo-mailjet'
 
 class MailjetInfos extends Component {
 
@@ -24,6 +25,12 @@ class MailjetInfos extends Component {
     this.props.onChange(key, e.target.value.trim())
   }
 
+  handleGoToMailjet = e => {
+    e.preventDefault()
+    e.stopPropagation()
+    shell.openExternal('https://app.mailjet.com/signup')
+  }
+
   render () {
 
     const {
@@ -39,8 +46,9 @@ class MailjetInfos extends Component {
     return (
       <div className='brand'>
         <Collapse isOpened={!isOpened} springConfig={{ stiffness: 300, damping: 30 }}>
-          <div className='d-f ai-c'>
-            <div className='d-f ai-fs jc-c' style={{ background: 'rgba(white, 0.1)' }}>
+          <div className='d-f ai-c p-20'>
+            <div className='d-f ai-c jc-c' style={{ background: 'rgba(white, 0.1)' }}>
+              <LogoMailjet height={30} className='mr-20' />
               <div className='mr-10 t-small' style={{ lineHeight: '18px' }}>
                 <span>{'Sending from '}<b className='us-t ff-m'>{SenderEmail}</b></span>
                 <br />
@@ -53,55 +61,68 @@ class MailjetInfos extends Component {
         </Collapse>
 
         <Collapse isOpened={isOpened} springConfig={{ stiffness: 300, damping: 30 }}>
-          <div className='flow-v-20'>
-            <div className='d-f ai-b'>
-              <div style={{ width: 150 }} className='fs-0 t-small'>
-                {'Mailjet API Key:'}
+          <div className='p-20'>
+            <div className='mb-20 d-f ai-c'>
+              <LogoMailjet height={20} className='mr-20 anim-mailjet' />
+              <div className='t-small' style={{ lineHeight: '18px' }}>
+                <span className='c-white'>
+                  {'MJML App uses the Mailjet API to send emails. '}
+                </span>
+                <a href='' onClick={this.handleGoToMailjet} className='a white'>
+                  {'Create your account'}
+                </a>
               </div>
-              <input
-                ref={n => this._firstInput = n}
-                className='fg-1'
-                value={APIKey}
-                onChange={this.handleChangeInput('APIKey')}
-                placeholder='Mailjet API Key'
-                type='text'
-              />
             </div>
-
-            <div className='d-f ai-b'>
-              <div style={{ width: 150 }} className='fs-0 t-small'>
-                {'Mailjet API Secret:'}
-              </div>
-              <input
-                className='fg-1'
-                value={APISecret}
-                onChange={this.handleChangeInput('APISecret')}
-                placeholder='Mailjet API Secret'
-                type='text'
-              />
-            </div>
-
-            <div className='d-f ai-b'>
-              <div style={{ width: 150 }} className='fs-0 t-small'>
-                {'Sender Email:'}
-              </div>
-              <div className='d-f fd-c fg-1'>
+            <div className='flow-v-20'>
+              <div className='d-f ai-b'>
+                <div style={{ width: 150 }} className='fs-0 t-small'>
+                  {'Mailjet API Key:'}
+                </div>
                 <input
-                  value={SenderEmail}
-                  onChange={this.handleChangeInput('SenderEmail')}
-                  placeholder='Sender Email'
+                  ref={n => this._firstInput = n}
+                  className='fg-1'
+                  value={APIKey}
+                  onChange={this.handleChangeInput('APIKey')}
+                  placeholder='Mailjet API Key'
                   type='text'
                 />
-                <div className='t-small mt-10 ta-r d-f ai-c flow-h-5'>
-                  <IconInfo />
-                  <div>
-                    {'Must be a verified sender. '}
-                  </div>
-                  <div
-                    className='a white'
-                    onClick={() => shell.openExternal('https://app.mailjet.com/account/sender')}
-                  >
-                    {'Learn more'}
+              </div>
+
+              <div className='d-f ai-b'>
+                <div style={{ width: 150 }} className='fs-0 t-small'>
+                  {'Mailjet API Secret:'}
+                </div>
+                <input
+                  className='fg-1'
+                  value={APISecret}
+                  onChange={this.handleChangeInput('APISecret')}
+                  placeholder='Mailjet API Secret'
+                  type='text'
+                />
+              </div>
+
+              <div className='d-f ai-b'>
+                <div style={{ width: 150 }} className='fs-0 t-small'>
+                  {'Sender Email:'}
+                </div>
+                <div className='d-f fd-c fg-1'>
+                  <input
+                    value={SenderEmail}
+                    onChange={this.handleChangeInput('SenderEmail')}
+                    placeholder='Sender Email'
+                    type='text'
+                  />
+                  <div className='t-small mt-10 ta-r d-f ai-c flow-h-5'>
+                    <IconInfo />
+                    <div>
+                      {'Must be a verified sender. '}
+                    </div>
+                    <div
+                      className='a white'
+                      onClick={() => shell.openExternal('https://app.mailjet.com/account/sender')}
+                    >
+                      {'Learn more'}
+                    </div>
                   </div>
                 </div>
               </div>
