9ec7ae41fcf259db85630bc28bbc77219cb62b48
meriadec <meriadec.pillet@gmail.com>
2017-05-19 10:18:55 +0200

    Pass className in Checkbox component

--- app/components/CheckBox/index.js
+++ app/components/CheckBox/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import cx from 'classnames'
 import PropTypes from 'prop-types'
 import IconUnchecked from 'react-icons/md/check-box-outline-blank'
 import IconChecked from 'react-icons/md/check-box'
@@ -24,12 +25,13 @@ class CheckBox extends Component {
       value,
       onChange,
       children,
+      className,
     } = this.props
 
     return (
       <div
         tabIndex={0}
-        className='Checkbox d-f ai-fs cu-d t-small focus'
+        className={cx(className, 'Checkbox d-f ai-fs cu-d t-small focus')}
         onKeyDown={this.handleKeyDown}
         onClick={() => onChange(!value)}
       >
