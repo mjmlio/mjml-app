7bbfd390b9c293292270194745f9f54452fcb582
cavrois cedric <cavrois.cedric@hotmail.fr>
2018-03-05 12:46:53 +0100

    update autocomplete & templates

--- src/templates/single-basic.js
+++ src/templates/single-basic.js
@@ -8,32 +8,29 @@ export default {
 
     <mj-attributes>
       <mj-text align="center" color="#555" />
-      <mj-container background-color="#eee" />
+      <mj-body background-color="#eee" />
       <mj-section background-color="#fff" />
     </mj-attributes>
 
   </mj-head>
 
   <mj-body>
-    <mj-container>
-      <mj-section>
+    <mj-section>
 
-        <mj-column>
-          <mj-text>
-            First column
-          </mj-text>
-        </mj-column>
+      <mj-column>
+        <mj-text>
+          First column
+        </mj-text>
+      </mj-column>
 
-        <mj-column>
-          <mj-text>
-            Second column
-          </mj-text>
-        </mj-column>
+      <mj-column>
+        <mj-text>
+          Second column
+        </mj-text>
+      </mj-column>
 
-      </mj-section>
-    </mj-container>
+    </mj-section>
   </mj-body>
-
 </mjml>`,
     },
   ],
