97b9cd9b6351572d910e56879c82cdf1247cce56
Eike Thies <thies.eike@gmail.com>
2018-05-13 14:50:18 +0200

    change path of tmp file for screenshots to respect relative paths

--- src/pages/Project/index.js
+++ src/pages/Project/index.js
@@ -31,7 +31,7 @@ import SendModal from './SendModal'
 import AddFileModal from './AddFileModal'
 import RemoveFileModal from './RemoveFileModal'
 
-import takeScreenshot from 'helpers/takeScreenshot'
+import { takeScreenshot, cleanUp } from 'helpers/takeScreenshot'
 
 @connect(
   state => ({
@@ -150,9 +150,11 @@ class ProjectPage extends Component {
     const [mobileWidth, desktopWidth] = [previewSize.get('mobile'), previewSize.get('desktop')]
 
     const [mobileScreenshot, desktopScreenshot] = await Promise.all([
-      takeScreenshot(preview.content, mobileWidth),
-      takeScreenshot(preview.content, desktopWidth),
+      takeScreenshot(preview.content, mobileWidth, this.state.path),
+      takeScreenshot(preview.content, desktopWidth, this.state.path),
     ])
+
+    await cleanUp(this.state.path)
 
     await Promise.all([
       fsWriteFile(pathModule.join(location.query.path, `${filename}-mobile.png`), mobileScreenshot),
