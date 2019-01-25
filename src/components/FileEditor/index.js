7149756c8898840be619148de6552d41f06a3258
KMcB <cavrois.cedric@hotmail.fr>
2019-01-25 18:14:36 +0100

    beautify rm empty lines, same config as in mjml

--- src/components/FileEditor/index.js
+++ src/components/FileEditor/index.js
@@ -46,8 +46,11 @@ function beautify(content) {
   return beautifyJS.html(content, {
     indent_size: 2, // eslint-disable-line camelcase
     wrap_attributes_indent_size: 2, // eslint-disable-line camelcase
+    max_preserve_newline: 0, // eslint-disable-line camelcase
+    preserve_newlines: false, // eslint-disable-line camelcase
   })
 }
+
 
 @connect(
   state => {
