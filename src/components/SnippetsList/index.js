0a765e4bdd2a4da6a32cc61c8a465e9fb5e11256
Cédric Cavrois <cedric.cavrois@ext.adeo.com>
2022-01-07 13:55:16 +0100

    ref #299 allow to filter snippets

--- src/components/SnippetsList/index.js
+++ src/components/SnippetsList/index.js
@@ -9,23 +9,40 @@ export default connect(state => ({
   settings: state.settings,
 }))(
   class SnippetsList extends Component {
+    state = {
+      filter: ''
+    }
+    
     render() {
       const { settings } = this.props
+      const { filter } = this.state
       const snippets = settings.get('snippets')
+      const snippetsFiltered = !filter ? snippets : snippets.filter(s => s.name.includes(filter))
 
       return (
         <div className="fg-1 d-b">
-          {snippets.map(s => {
-            return (
-              <SnippetItem
-                key={s.name}
-                className="SnippetItem"
-                name={s.name}
-                trigger={s.trigger}
-                content={s.content}
-              />
-            )
-          })}
+          <div className="mb-20">
+            <input
+              className="fg-1"
+              onChange={e => this.setState({ filter: e.target.value })}
+              placeholder="Filter by name"
+              value={filter}
+              type="text"
+            />
+          </div>
+          <div>
+            {snippetsFiltered.map(s => {
+              return (
+                <SnippetItem
+                  key={s.name}
+                  className="SnippetItem"
+                  name={s.name}
+                  trigger={s.trigger}
+                  content={s.content}
+                />
+              )
+            })}
+          </div>
         </div>
       )
     }
